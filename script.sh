09d2ce4fcec3c5bdc602eadf09ec443365bdb7a2
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-09 18:18:05 -0500

    x

--- script.sh
+++ script.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+workspace_path=$(pwd)
+
+# hashes:
+curr_hash=$(git rev-parse HEAD)
+curr_json_out=./$curr_hash
+
+echo $curr_hash . "->" $curr_json_out
+
 # assumptions made:
 #   - java is installed (should be fine)
 #   - install_bazel_diff was already invoked, and this script is being executed in the same location
 #   - the WORKSPACE file exists at the root of the repo.
-java -jar bazel-diff.jar generate-hashes --workspacePath=$(pwd)
+java -jar bazel-diff.jar generate-hashes --workspacePath=$workspace_path $curr_json_out
+
+cat $curr_json_out
