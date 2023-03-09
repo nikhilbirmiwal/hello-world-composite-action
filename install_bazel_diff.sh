4a5d3f92d39b73f1ffd592d3f7e9b96957451f6d
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-09 15:05:37 -0500

    v21

---
+++ install_bazel_diff.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -Lo bazel-diff.jar https://github.com/Tinder/bazel-diff/releases/latest/download/bazel-diff_deploy.jar
--- script.sh
+++ script.sh
@@ -1,12 +1,5 @@
-bazel_path=$(which bazel)
-workspace_path=$GITHUB_ACTION_PATH/WORKSPACE # should not be assumed
-curr_sha=$(git rev-parse HEAD)
+#!/usr/bin/env bash
 
-echo $bazel_path
-echo $workspace_path
-echo $curr_sha
+find .
 
-echo "Running command"
-
-cd tools/ # should be a known tmp directory. maybe trunk-prefixed?
-bazel run :bazel-diff -- --version # should we even be cloning the repo into the directory?
+java -jar bazel-diff.jar -h
