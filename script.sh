8f4b2196e4c0fb3e6430c04ef1b72bcb924b705b
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-09 13:26:35 -0500

    v16

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
-echo "pwd"
-pwd
-
 find .
 
 bazel_path=$(which bazel)
 workspace_path=//...
+curr_sha=$(git rev-parse HEAD)
 
 echo $bazel_path
 echo $workspace_path
 echo $curr_sha
+
+echo "Running command"
 
 ./tools/bazel-diff-example.sh $bazel_path $workspace_path $curr_sha $curr_sha
