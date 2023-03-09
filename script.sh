148155e5c5448ed8324d65653ca1c65f94697764
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-09 17:44:32 -0500

    v27

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo "pwd"
+pwd
+
+echo "contents"
+find .
+
 bazel_path=$(which bazel)
 
 java -jar bazel-diff.jar generate-hashes --bazelPath=$bazel_path --workspacePath=$GITHUB_ACTION_PATH
