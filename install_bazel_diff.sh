a6f1394154ce16e7517d467cb710a24afd7fa6f2
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-09 17:53:09 -0500

    v29

--- install_bazel_diff.sh
+++ install_bazel_diff.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 pwd
-find .
 
 curl -Lo bazel-diff.jar https://github.com/Tinder/bazel-diff/releases/latest/download/bazel-diff_deploy.jar
+
+find .
--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ find .
 
 bazel_path=$(which bazel)
 
-java -jar bazel-diff.jar generate-hashes --bazelPath=$bazel_path --workspacePath=$GITHUB_ACTION_PATH
+java -jar bazel-diff.jar generate-hashes --bazelPath=$bazel_path --workspacePath=$(pwd)
