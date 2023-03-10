e3b15267032f27c7753a8e6898089533f27f0ff3
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-10 16:35:47 -0500

    x

--- script.sh
+++ script.sh
@@ -4,17 +4,24 @@
 # TODO: Should be overridable
 workspace_path=$(pwd)
 
-find .
+# Assumption: a user's bazelrc file exists at the root of the caller's repo, next to the WORKSPACE
+# TODO: Should be overridable
+BAZEL_RC_PATH=.bazelrc 
 
 # hashes:
 HEAD_OUT=./$HEAD_SHA
 BASE_OUT=./$BASE_SHA
 
+# Debug Logs
+echo $HEAD_SHA
+echo $BASE_SHA
+echo $BAZEL_RC_PATH
+
 # Fetch the latest commits from Git
 git fetch 
 
 git checkout "$HEAD_SHA"
-java -jar bazel-diff.jar generate-hashes --bazelCommandOptions=--bazelrc=./.bazelc  --verbose --workspacePath=$workspace_path $HEAD_OUT 
+java -jar bazel-diff.jar generate-hashes --bazelCommandOptions=--bazelrc=$BAZEL_RC_PATH  --verbose --workspacePath=$workspace_path $HEAD_OUT 
 
 git checkout "$BASE_SHA"
 java -jar bazel-diff.jar generate-hashes --workspacePath="$workspace_path" "$BASE_OUT"
