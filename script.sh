f85ecf40b4dd8013f6431e3807733e4abcea3f29
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-10 17:01:05 -0500

    v7

--- script.sh
+++ script.sh
@@ -17,16 +17,10 @@ echo $HEAD_SHA
 echo $BASE_SHA
 echo $BAZEL_RC_PATH
 
-# Fetch the latest commits from Git
-git fetch 
+git rev-parse HEAD
+java -jar bazel-diff.jar generate-hashes --workspacePath=$workspace_path $HEAD_OUT 
 
-git checkout "$HEAD_SHA"
-java -jar bazel-diff.jar generate-hashes --bazelStartupOptions=--bazelrc=$BAZEL_RC_PATH  --workspacePath=$workspace_path $HEAD_OUT 
-
-git checkout "$BASE_SHA"
-java -jar bazel-diff.jar generate-hashes --workspacePath="$workspace_path" "$BASE_OUT"
-
-java -jar bazel-diff.jar get-impacted-targets --startingHashes="$BASE_OUT" --finalHashes="$HEAD_OUT"
+cat $HEAD_OUT
 
 # TODO: 
 # - Invoke the /uploadAffectedTargets API
