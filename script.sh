791539eccd60532636f5e7c43cae01b701ce20fa
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-11 11:07:39 -0500

    x

--- script.sh
+++ script.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-pwd
-echo $GITHUB_ACTION_PATH
-
 # Assumption: a WORKSPACE file exists at the root of the caller's repo
 # TODO: Should be overridable
-workspace_path=$GITHUB_ACTION_PATH
+workspace_path=$(pwd)
 
 # Assumption: a user's bazelrc file exists at the root of the caller's repo, next to the WORKSPACE
 # TODO: Should be overridable
@@ -20,10 +17,8 @@ echo $HEAD_SHA
 echo $BASE_SHA
 echo $BAZEL_RC_PATH
 
-find .
-
 # TODO: This shouldn't be hardcoded to tools.
-bazel run //tools:bazel-diff generate-hashes --workspacePath=$workspace_path 
+bazel run //tools:bazel-diff generate-hashes -- --workspacePath=$workspace_path 
 
 # TODO: 
 # - Invoke the /uploadAffectedTargets API
