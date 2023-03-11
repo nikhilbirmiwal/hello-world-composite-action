6afabcf0e7359c939232999be859ce05773ffea7
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-11 11:23:27 -0500

    x

--- script.sh
+++ script.sh
@@ -17,10 +17,11 @@ echo $HEAD_SHA
 echo $BASE_SHA
 echo $BAZEL_RC_PATH
 
-cd tools/ 
+cd /tmp/tools
+
+find .
 
 bazel run //:bazel-diff -- -h
-bazel run //:bazel-diff -- generate-hashes --workspacePath=$workspace_path 
 
 # TODO: 
 # - Invoke the /uploadAffectedTargets API
