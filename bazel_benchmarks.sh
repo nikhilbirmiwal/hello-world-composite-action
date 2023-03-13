7ac76d13e0e25cbffb5bd3043904b882204cbac2
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-13 12:59:06 -0400

    add qupte

--- bazel_benchmarks.sh
+++ bazel_benchmarks.sh
@@ -2,4 +2,4 @@ echo "Timing a full bazel query: source files"
 time bazel query --output streamed_proto --order_output=no --keep_going "kind('source file', //...:all-targets)"
 
 echo "Timing a full bazel query: external + all_targets"
-time bazel query --output streamed_proto --order_output=no --keep_going "//external:all-targets' + '//...:all-targets'"
+time bazel query --output streamed_proto --order_output=no --keep_going "'//external:all-targets' + '//...:all-targets'"
