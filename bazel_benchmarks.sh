50991faa6542de537fb0548399338002d85122ab
Nikhil Birmiwal <nikhil@trunk.io>
2023-03-13 13:33:35 -0400

    x

--- bazel_benchmarks.sh
+++ bazel_benchmarks.sh
@@ -1,5 +1,5 @@
 echo "Timing a full bazel query: source files"
-time bazel query --output streamed_proto --order_output=no --keep_going "kind('source file', //...:all-targets)"
+time bazel query --output streamed_proto --order_output=no --keep_going "kind('source file', //...:all-targets)" > out1
 
 echo "Timing a full bazel query: external + all_targets"
-time bazel query --output streamed_proto --order_output=no --keep_going "'//external:all-targets' + '//...:all-targets'"
+time bazel query --output streamed_proto --order_output=no --keep_going "'//external:all-targets' + '//...:all-targets'" > out2
